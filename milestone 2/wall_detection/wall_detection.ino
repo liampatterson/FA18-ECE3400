5de6f375913c74e6809ea6136b31ca445fd9562f
kmartucci01 <kmm398@cornell.edu>
2018-10-01 13:49:56 -0400

    wall detection 10.1

--- milestone 2/wall_detection/wall_detection.ino
+++ milestone 2/wall_detection/wall_detection.ino
@@ -1,38 +1,40 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin( 9600 );
-  pinMode(7, OUTPUT); // red LED
-  pinMode(6, OUTPUT); //white LED
+  pinMode(6, OUTPUT); // red LED
+  pinMode(7, OUTPUT); //white LED
   pinMode(5, OUTPUT); //green LED
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int LeftDistance = analogRead(A3);
+  int MiddleDistance = analogRead(A5);
   int RightDistance = analogRead(A4);
-  String l = "left: ";
-  String r = "        right: ";
-  Serial.println( l + LeftDistance + r + RightDistance );
-  if(LeftDistance > 200){
+  
+  String left = "left: ";
+  String middle = "     middle:";
+  String right = "        right: ";
+  Serial.println( left + LeftDistance + middle + MiddleDistance + right + RightDistance );
+  //green
+  if(LeftDistance < 200){
     digitalWrite(5, HIGH);
   }
   else{
     digitalWrite(5, LOW);
   }
-  if(RightDistance > 200){
+  //red
+  if(RightDistance < 200){
     digitalWrite(6, HIGH);
   }
   else{
     digitalWrite(6, LOW);
   }
+  //white
+  if(MiddleDistance < 200){
+    digitalWrite(7, HIGH);
+  }
+  else{
+    digitalWrite(7, LOW);
+  }
 }
-
-// look at two LEDs like Binary
-// Left is White (blinks green)
-// Right is Red 
-// White = bit 1 (MSB)
-// Red = bit 0 (LSB)
-// 00 means no walls anywhere
-// 01 means left wall detected
-// 10 means right wall detected
-// 11 means front wall detected
