61b7b427d2db100167178ef4f7e96033c27b4340
Emma Kaufman <eck66@cornell.edu>
2018-11-05 17:24:46 -0500

    update image processor

--- lab4/Lab4_FPGA_Template/IMAGE_PROCESSOR.v
+++ lab4/Lab4_FPGA_Template/IMAGE_PROCESSOR.v
@@ -24,16 +24,18 @@ input			VGA_VSYNC_NEG;
 
 output reg [1:0] RESULT;
 
-reg [9:0] BLUECOUNT;
-reg [9:0] REDCOUNT;
+reg [15:0] BLUECOUNT = 0;
+reg [15:0] REDCOUNT = 0;
+reg [15:0] REDCOUNTTEMP = 0;
+reg [15:0] BLUECOUNTTEMP = 0;
 
-always @ (posedge CLK) begin
+always @ (posedge CLK, negedge VGA_VSYNC_NEG) begin
 		if (!VGA_VSYNC_NEG) begin
-			if (BLUECOUNT > REDCOUNT && BLUECOUNT > 10'b0001100100) begin //100 is arbitrary, can change
-				RESULT = 2'b01; //when color is 01, blue
+			if (BLUECOUNTTEMP > REDCOUNTTEMP) begin //100 is arbitrary, can change && BLUECOUNT > 10'b0001100100
+				RESULT = 2'b10; //when color is 10, blue
 			end
-			else if (REDCOUNT > BLUECOUNT && REDCOUNT > 10'b0001100100) begin
-				RESULT = 2'b10; //when color is 10, red
+			else if (REDCOUNTTEMP > BLUECOUNTTEMP) begin //&& REDCOUNT > 10'b0001100100
+				RESULT = 2'b01; //when color is 01, red
 			end
 			else begin
 				RESULT = 2'b00; //no color detected
@@ -42,15 +44,19 @@ always @ (posedge CLK) begin
 			REDCOUNT = 0;
 		end
 		else begin
-			if (VGA_PIXEL_Y >= 10'b0001000110 && VGA_PIXEL_Y <= 10'b0001001010) begin
-				if (PIXEL_IN[7:6] > PIXEL_IN[1:0] && PIXEL_IN[7:6] > PIXEL_IN[4:3]) begin
-					REDCOUNT = REDCOUNT + 1'b1;
-				end
-				else if (PIXEL_IN[1:0] > PIXEL_IN[7:6] && PIXEL_IN[1:0] > PIXEL_IN[4:3]) begin
-					BLUECOUNT = BLUECOUNT + 1'b1;
-				end
+			//if (VGA_PIXEL_Y >= 10'b0001000110 && VGA_PIXEL_Y <= 10'b0001001010) begin
+				//RESULT = 2'b10;
+			//REDCOUNT = REDCOUNT + 1;
+			if (PIXEL_IN[7:6] > PIXEL_IN[1:0] && PIXEL_IN[7:6] > PIXEL_IN[4:3]) begin
+				REDCOUNT = REDCOUNT + 1'b1;
 			end
+			else if (PIXEL_IN[1:0] > PIXEL_IN[7:6] && PIXEL_IN[1:0] > PIXEL_IN[4:3]) begin
+				BLUECOUNT = BLUECOUNT + 1'b1;
+			end
+			REDCOUNTTEMP = REDCOUNT;
+			BLUECOUNTTEMP = BLUECOUNT;
 		end
+		//end
 end
 
 
