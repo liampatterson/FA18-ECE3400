afc8839a10deeca379192a7ac0a86754e1606407
liampatterson <patterson.liam@gmail.com>
2018-11-11 14:21:38 -0500

    added helper

--- milestone 3/high_level_algo/high_level_algo.ino
+++ milestone 3/high_level_algo/high_level_algo.ino
@@ -8,47 +8,90 @@ void loop() {
 
 }
 
-visited = []; //frontier set
-
-startNode = (0,0);
-stack.push(startNode);
-
-
-//not sure how to write possibleForwardNode etc, need to know orientation and coordinate mapping stuff
-while(!stack.empty()){
-  curent = stack.pop;
-  current.visited = true;
-  //unvisited
-  if(noFrontWall && possibleForwardNode.visited == false){
-    Straight();
-    stack.push(possibleForwardNode);
+void DFSimplementation( void ) {
+  visited = []; //frontier set
+  
+  startNode = (0,0);
+  stack.push(startNode);
+  
+  
+  //not sure how to write possibleForwardNode etc, need to know orientation and coordinate mapping stuff
+  while(!stack.empty()){
+    current = stack.pop;
+    current.visited = true;
+    decodePossibleSets( orientation );
+    //unvisited
+    if(noFrontWall && possibleForwardNode.visited == false){
+      Straight();
+      stack.push(possibleForwardNode);
+    }
+    else if(noLeftWall && possibleLeftNode.visited == false){
+      goLeft();
+      stack.push(possibleLeftNode);
+    }
+    else if(noRightWall && possibleRightNode.visited == false){
+      goRight();
+      stack.push(possibleRightNode);
+    }
+    else{
+      turnAround();
+    }
+    //has to visit a visited node
+    if(noFrontWall){
+      Straight();
+      stack.push(possibleForwardNode);
+    }
+    else if(noLeftWall){
+      goLeft();
+      stack.push(possibleLeftNode);
+    }
+    else if(noRightWall){
+      goRight();
+      stack.push(possibleRightNode);
+    }
+    else{
+      turnAround();
+    }
+  
   }
-  else if(noLeftWall && possibleLeftNode.visited == false){
-    goLeft();
-    stack.push(possibleLeftNode);
-  }
-  else if(noRightWall && possibleRightNode.visited == false){
-    goRight();
-    stack.push(possibleRightNode);
-  }
-  else{
-    turnAround();
-  }
-  //has to visit a visited node
-  if(noFrontWall){
-    Straight();
-    stack.push(possibleForwardNode);
-  }
-  else if(noLeftWall){
-    goLeft();
-    stack.push(possibleLeftNode);
-  }
-  else if(noRightWall){
-    goRight();
-    stack.push(possibleRightNode);
-  }
-  else{
-    turnAround();
-  }
-
 }
+
+//assumes coordinates are in format char xVal,yVal
+void decodePossibleSets( Orientation orientation, byte currentCoords ) {
+  //00010001 is coordinate (1,1)
+  //00100001 is coordinate (2,1)
+  int xVal = ( currentCoords >> 4 )
+  int yVal = ( currentCoords && B00001111 )
+  
+  switch( orientation ) {
+    case north_up:
+      possibleForwardNode = intToByte( xVal, yVal+1 );
+      possibleLeftNode = intToByte( xVal-1, yVal );
+      possibleRightNode = intToByte( xVal+1, yVal );
+      break;
+    case north_left:
+      possibleForwardNode = intToByte( xVal+1, yVal );
+      possibleLeftNode = intToByte( xVal, yVal+1 );
+      possibleRightNode = intToByte( xVal, yVal-1 );
+      break;
+    case north_back:
+      possibleForwardNode = intToByte( xVal, yVal-1 );
+      possibleLeftNode = intToByte( xVal+1, yVal );
+      possibleRightNode = intToByte( xVal-1, yVal );
+      break;
+    case north_right:
+      possibleForwardNode = intToByte( xVal-1, yVal );
+      possibleLeftNode = intToByte( xVal, yVal-1 );
+      possibleRightNode = intToByte( xVal, yVal+1 );
+      break;
+    default:
+      break;
+  }
+}
+
+byte intToByte( int xVal, int yVal ){
+  byte returnVal;
+  returnVal = ( xVal << 4 ) || ( yVal % 15);
+  return returnVal;
+}
+
